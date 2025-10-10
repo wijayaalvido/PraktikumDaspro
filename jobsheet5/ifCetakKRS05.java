107fdb5cc2bbe2a270edbe46d6980efe25bcc513
Alvido Wijaya <wijayaalvido@gmail.com>
2025-10-10 08:41:19 +0700

    Perobaan 1

---
+++ jobsheet5/ifCetakKRS05.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class ifCetakKRS05 {
+public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    System.out.println("--- Cetak KRS SIAKAD ---");
+    System.out.print("Apakah UKT sudah lunas? (true/false): ");
+    boolean uktLunas = sc.nextBoolean();
+
+    if (uktLunas) {
+        System.out.println("Pembayaran UKT terferivikasi");
+        System.out.println("Silahkan cetak KRS dan minta tanda tangan DPA");
+    
+        }
+    }
+}
