97481c864838cfc09e588b5ba072f19d014d9fd1
Alvido Wijaya <wijayaalvido@gmail.com>
2025-10-10 17:38:51 +0700

    Tugas 1

--- jobsheet5/ifCetakKRS05.java
+++ jobsheet5/ifCetakKRS05.java
@@ -9,11 +9,16 @@ public static void main(String[] args) {
     boolean uktLunas = sc.nextBoolean();
 
     if (uktLunas) {
-        System.out.println("Pembayaran UKT terferivikasi");
-        System.out.println("Silahkan cetak KRS dan minta tanda tangan DPA");
-        } else {
-        System.out.println("Registrasi ditolak");
-        System.out.println("Silahkan lunasi UKT terlebih dahulu");
+        //System.out.println("Pembayaran UKT terferivikasi");
+        //System.out.println("Silahkan cetak KRS dan minta tanda tangan DPA");
+        //} else {
+        //System.out.println("Registrasi ditolak, Silakan lunasi UKT terlebih dahulu");
+
+        String pesan = uktLunas
+                ? "Pembayaran UKT terferivikasi\nSilahkan cetak KRS dan minta tanda tangan DPA"
+                : "Registrasi ditolak\nSilahkan lunasi UKT terlebih dahulu";
+
+        System.out.println(pesan);
         }
     }
 }
